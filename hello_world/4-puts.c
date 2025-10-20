406e68b33477106b3d4f9d65f2620220a3b4f5b7
MinoTankesu <mino.fage@gmail.com>
2025-10-20 13:40:45 +0000

    Modifs de 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) /* Petit programme qui permet de print */
+int main(void) // Petit programme qui permet de print
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
