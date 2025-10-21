ac838bb42a94cb590091567b4541a41195cd63e0
MinoTankesu <mino.faget@gmail.com>
2025-10-21 07:15:42 +0000

    Ajout de 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**                                                                                                                                                                                                                                         
+ * main - Petit programme qui permet de print                                                                                                                                                                                               
+ * Return: Bah tu print                                                                                                                                                                                                                     
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
