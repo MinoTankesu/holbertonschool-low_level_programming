7de4e8ad8157a0de7aa04775bea7f53fc8a8f392
MinoTankesu <mino.faget@gmail.com>
2025-10-22 12:18:23 +0000

    Modifs de 6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 
 /**
 * _abs - Calcule la valeur absolue
+* @n: pour la valeur absolue
 * Return: 0
 */
-
 int _abs(int n)
 {
 if (n < 0)
-return -n;
+return (-n);
 else
-return n;
+return (n);
 }
