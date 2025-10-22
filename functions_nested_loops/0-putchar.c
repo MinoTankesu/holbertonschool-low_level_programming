42741b9e520a3f7381b98d52a0f2aebd224dbaa7
MinoTankesu <mino.faget@gmail.com>
2025-10-22 07:53:49 +0000

    Modifs de 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
+/**
+* main - Petit programme pour print _putchar avec un retour a la ligne
+* Return: 0
+*/
+
 int main(void)
 {
-    write(1, "_putchar\n", 9);
-    return 0;
+write(1, "_putchar\n", 9);
+return (0);
 }
