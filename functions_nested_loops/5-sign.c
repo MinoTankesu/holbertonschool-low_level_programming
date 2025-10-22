698a10db7aee0e3fc8b38bdc2cd0614a6be2a48c
MinoTankesu <mino.faget@gmail.com>
2025-10-22 11:55:29 +0000

    Modifs de 5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
-* print_sign - Petit programme pour return 1 si au dessus de 0, 0 si egal a 0, ou -1 si plus petit que 0
+* print_sign - Petit programme pour return 1 si au dessus de 0
+* 0 si egal a 0
+* -1 si plus petit que 0
 * @n: Pour tester l'alphabet
 * Return: 0
 */
@@ -10,17 +12,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+_putchar('+');
 return (1);
 }
 else if (n < 0)
 {
-putchar('-');
+_putchar('-');
 return (-1);
 }
 else
 {
-putchar('0');
+_putchar('0');
 return (0);
 }
 }
