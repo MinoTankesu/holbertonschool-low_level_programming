01d609c0cdb076e44d38f220fc202136f5acd292
MinoTankesu <mino.faget@gmail.com>
2025-10-22 09:42:01 +0000

    Modifs de 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
 * _islower - Petit programme pour return 1 si minuscule sinon 0 si majuscule
+* c - Pour tester l'alphabet
 * Return: 0
 */
 
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
-return(1);
+return (1);
 else
-return(0);
+return (0);
 }
