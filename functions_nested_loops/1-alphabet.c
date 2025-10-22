0eb4c8adf75e3375ec4b07a7eca089a4738f6e74
MinoTankesu <mino.faget@gmail.com>
2025-10-22 08:04:52 +0000

    Ajout de 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* main - Petit programme pour print l'alphabet avec un retour a la ligne
+* Return: 0
+*/
+
+int main(void)
+{
+write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+return (0);
+}
