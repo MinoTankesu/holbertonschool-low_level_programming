c3fa26637c0bf298c81c1c3c130a3fe9b6564f8c
MinoTankesu <mino.faget@gmail.com>
2025-10-27 08:48:41 +0000

    Modifs de 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+* _isupper - Programme pour check si la lettre est minuscule ou majuscule.
+* Return: 0 Si minuscule, sinon majuscule.
+* @c: ...
+*/
+
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
