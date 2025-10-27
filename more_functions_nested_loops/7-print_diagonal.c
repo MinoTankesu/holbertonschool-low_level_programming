1c18fb8b78d50a269477fe18c7dd9607b77f226e
MinoTankesu <mino.faget@gmail.com>
2025-10-27 13:43:29 +0000

    Modifs de 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,7 @@
 void print_diagonal(int n)
 {
 int i;
+int j;
 if (n <= 0)
 {
 _putchar('\n');
@@ -17,6 +18,10 @@ else
 {
 for (i = 0; i < n; i++)
 {
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
 _putchar(92);
 _putchar('\n');
 }
