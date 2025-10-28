dd1f25cc910f1c504407439cc0c696a7ebec35f1
MinoTankesu <mino.faget@gmail.com>
2025-10-28 10:09:12 +0000

    Modifs de 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,6 +10,7 @@ void print_triangle(int size)
 {
 int i;
 int j;
+int space;
 if (size <= 0)
 {
 _putchar('\n');
@@ -18,6 +19,10 @@ else
 {
 for (i = 1; i <= size; i++)
 {
+for (space = size - i; space > 0; space--)
+{
+_putchar(' ');
+}
 for (j = 1; j <= i; j++)
 {
 _putchar(35);
