e319da7d87c928a52835ae8183c824e7bfdf0b41
MinoTankesu <mino.faget@gmail.com>
2025-10-28 11:20:43 +0000

    Modifs de 2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _strlen - Programme pour return la longueur de la string.
+* Return: 0
+*/
+
+int _strlen(char *s)
+{
+int longueur;
+for (longueur = 0; longueur != *s; longueur++)
+{
+return (longueur);
+}
+return (0);
+}
