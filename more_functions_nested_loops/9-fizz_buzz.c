1985b83c0212a0026fc202974ab84f1a1e7faf1f
MinoTankesu <mino.faget@gmail.com>
2025-10-28 09:33:55 +0000

    Modifs de 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,22 @@ int j;
 for (j = 1; j <= 100; j++)
 {
 if (j % 3 == 0 && j % 5 == 0)
+{
 printf("FizzBuzz ");
+}
 else if (j % 3 == 0)
+{
 printf("Fizz ");
+}
 else if (j % 5 == 0)
+{
 printf("Buzz ");
+}
 else
+{   
 printf("%d ", j);
 }
+}
+printf("\n");
 return (0);
 }
