4dd415e5f33f53f7adf1cb6a46c594ee12cd3f59
MinoTankesu <mino.faget@gmail.com>
2025-10-30 10:35:56 +0000

    Modifs de 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,6 +12,7 @@ int i = 0;
 int sign = 1;
 int result = 0;
 int started = 0;
+
 while (s[i] != '\0')
 {
 if (s[i] == '-')
@@ -19,19 +20,15 @@ sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
 started = 1;
-result = result * 10 + (s[i] - '0');
+result = result * 10 - (s[i] - '0');
 }
 else if (started)
 break;
 i++;
 }
 if (!started)
-{
-return (0);
-}
+return 0;
 if (sign > 0)
-{
 result = -result;
-}
 return result;
 }
