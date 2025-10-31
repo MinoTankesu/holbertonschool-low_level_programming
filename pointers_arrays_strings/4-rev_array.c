1fc905a579a4fd6cacf7e25e7cd5abad4fc9d126
MinoTankesu <mino.faget@gmail.com>
2025-10-31 09:41:02 +0000

    Modifs de 4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+* reverse_array - Programme pour reverse le contenue des 2 integers.
+*@a: le tableau.
+*@n: longueur du tableau.
+* Return: ...
+*/
+
 void reverse_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n/2; i++)
+for (i = 0; i < n / 2; i++)
 {
 int temp = a[i];
 a[i] = a[n - 1 - i];
