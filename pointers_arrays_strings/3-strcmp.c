5c0d07ef3871577f1af5492f88ebdfe2b07c4577
MinoTankesu <mino.faget@gmail.com>
2025-10-31 09:07:54 +0000

    Modifs de 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,18 @@
 #include "main.h"
 
+/**
+* _strcmp - Programme pour compare 2 strings.
+*@s1: premiere string.
+*@s2: deuxieme string.
+* Return: la comparaison.
+*/
+
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && *s1 == *s2) { ++s1; ++s2; }
-return (int)(unsigned char)(*s1) - (int)(unsigned char)(*s2);
+while (*s1 && *s1 == *s2)
+{
+++s1;
+++s2;
+}
+return ((int)(unsigned char)(*s1) - (int)(unsigned char)(*s2));
 }
