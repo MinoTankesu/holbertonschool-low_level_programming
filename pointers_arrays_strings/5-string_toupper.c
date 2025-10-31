c969643299511518adff052591c5adc58cc386cd
MinoTankesu <mino.faget@gmail.com>
2025-10-31 10:01:44 +0000

    Modifs de 5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+* *string_toupper - Programme pour changer les lettres minuscules en majuscule.
+*@str: le tableau.
+* Return: str.
+*/
+
 char *string_toupper(char *str)
 {
 int i;
 for (i = 0; str[i] != '\0'; i++)
-{ 
+{
 if (str[i] >= 'a' && str[i] <= 'z')
-str[i] =(int)str[i] - 32;
+str[i] = (int)str[i] - 32;
 }
 return (str);
 }
