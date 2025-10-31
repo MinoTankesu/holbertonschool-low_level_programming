df1609a8250c47ab4892e0360622995a21d5bca4
MinoTankesu <mino.faget@gmail.com>
2025-10-31 12:41:05 +0000

    Ajout de 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,10 +3,25 @@
 char *cap_string(char *str)
 {
 int i;
+int word = 1;
 for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] >= 'a' && str[i] <= 'z')
+{
+if (word)
+{
 str[i] = (int)str[i] - 32;
+word = 0;
+}
+}
+else if (str[i] >= 'A' && str[i] <= 'Z')
+{
+word = 0;
+}
+else
+{
+word = 1;
+}
 }
 return (str);
 }
