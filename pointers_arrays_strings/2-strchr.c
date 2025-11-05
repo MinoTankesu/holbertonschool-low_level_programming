bf0244311704e19a1e58536ab5fc5a4b6827a2ea
MinoTankesu <mino.faget@gmail.com>
2025-11-05 10:32:58 +0000

    Modifs de 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,17 @@
 char *_strchr(char *s, char c)
 {
 int i = 0;
-while (s[i] != '\0' && s[i] != c)
+while (s[i] != '\0')
 {
-i++;
-if (s[i] == '\0' || s[i] == c || c == '\0')
+if (s[i] == c)
 {
 return (&s[i]);
 }
+i++;
+}
+if (c == '\0')
+{
+return (&s[i]);
 }
 return (NULL);
 }
