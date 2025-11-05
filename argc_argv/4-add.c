165fc52f66ead4888bfa83a615f7e330ddd21727
MinoTankesu <mino.faget@gmail.com>
2025-11-05 10:04:15 +0000

    Ajout de 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* main - Print le nombre d'argument.
+* @argc: argc.
+* @argv: argv.
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+int j;
+int sum = 0;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+for (i = 1; i < argc; i++)
+{
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
