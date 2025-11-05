9c8765b8e53cc259f16d251998e1354007888e10
MinoTankesu <mino.faget@gmail.com>
2025-11-05 09:42:58 +0000

    Ajout de 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,10 +9,15 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
+int num1;
+int num2;
+if (argc != 3)
 {
-printf("%s\n", argv[i]);
+printf("Error\n");
+return (1);
 }
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+printf("%d\n", num1 *num2);
 return (0);
 }
